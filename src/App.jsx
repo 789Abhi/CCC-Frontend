5b49eabf2da61a4b603dd76bfebc86be01041ed5
Abhishek <abhishektk18@gmail.com>
2025-05-06 22:27:45 +0530

    New Logic Updated with Live 1.2.4

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ErrorBoundary from './ErrorBoundary'
 
 function App() {
   return (
-    <section className='bg-custom-gradient h-screen '>
+    <section className='bg-custom-gradient h-screen -ml-5'>
       <div className='container mx-auto py-[26px]'>
        
         <Header/>
