62501924fd8cd807b1eaf84b0302c6c40a5ddbe7
Abhishek <abhishektk18@gmail.com>
2025-07-12 19:28:10 +0530

    Plugin Menu connection with the cms Menu 3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log('CCC: React app starting...');
+console.log('CCC: Looking for root element:', document.getElementById('root'));
+
+try {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    console.log('CCC: Root element found, mounting React app...');
+    createRoot(rootElement).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    );
+    console.log('CCC: React app mounted successfully');
+  } else {
+    console.error('CCC: Root element not found!');
+  }
+} catch (error) {
+  console.error('CCC: Error mounting React app:', error);
+}
