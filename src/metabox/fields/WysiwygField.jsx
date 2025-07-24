18abae28d8766f195c1ebf5d26453f665fa2809a
Abhishek <abhishektk18@gmail.com>
2025-07-24 21:00:00 +0530

    wysiwyg Editor  5

--- src/metabox/fields/WysiwygField.jsx
+++ src/metabox/fields/WysiwygField.jsx
@@ -26,8 +26,8 @@ function WysiwygField({ label, value, onChange, required, error, editorId }) {
       window.wp.editor.initialize(editorId, {
         tinymce: {
           wpautop: true,
-          plugins: 'charmap colorpicker hr lists paste tabfocus textcolor  wordpress wpautoresize wpeditimage wpemoji wpgallery wplink wptextpattern',
-          toolbar1: 'formatselect bold italic bullist numlist blockquote alignleft aligncenter alignright link unlink wp_more spellchecker  wp_adv',
+          plugins: 'charmap colorpicker hr lists paste tabfocus textcolor fullscreen wordpress wpautoresize wpeditimage wpemoji wpgallery wplink wptextpattern',
+          toolbar1: 'formatselect bold italic bullist numlist blockquote alignleft aligncenter alignright link unlink wp_more spellchecker fullscreen wp_adv',
           toolbar2: 'strikethrough hr forecolor pastetext removeformat charmap outdent indent undo redo wp_help',
         },
         quicktags: true,
