39303890165af67a307dceb8826cd7dbc0695990
Abhishek <abhishektk18@gmail.com>
2025-08-26 17:35:16 +0530

    new reset code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -101,7 +101,7 @@ function Header() {
                   alt="CCC Logo"
                 />
               </div>
-              <div className="flex-1 px-10 py-7 bg-customGray text-center rounded-custom">
+              <div className="flex-1 px-10 py-8 bg-customGray text-center rounded-custom">
                 <h1 className="text-4xl font-bold text-bgPrimary">Custom Craft Components </h1>
               </div>
             </div>
