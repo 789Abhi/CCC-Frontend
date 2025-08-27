6e107bd70d135b016985a9d6de18e64160629983
Abhishek <abhishektk18@gmail.com>
2025-08-27 20:34:39 +0530

    website template Design 29

--- src/components/ComponentList.jsx
+++ src/components/ComponentList.jsx
@@ -1310,7 +1310,7 @@ const ComponentList = () => {
                 <svg className="h-[30px] w-[30px]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                Use ChatGPT
+                Use AI
               </button>
               
                              <div className="relative export-all-dropdown-container">
@@ -1524,7 +1524,7 @@ const ComponentList = () => {
                          className="w-[25px] h-[25px] cursor-pointer text-blue-600 hover:text-blue-800 transition-colors duration-200 relative export-dropdown-container"
                          title="Export Component"
                        >
-                         <Download 
+                         <Upload 
                            className="w-[25px] h-[25px]" 
                            onClick={() => {
                              setShowExportTypeDropdown(prev => ({
@@ -1575,7 +1575,7 @@ const ComponentList = () => {
                            setShowImportModal(true)
                          }}
                        >
-                         <Upload className="w-[25px] h-[25px]" />
+                         <Download className="w-[25px] h-[25px]" />
                        </div>
                        <img
                          onClick={() => handleDeleteComponent(comp.id)}
